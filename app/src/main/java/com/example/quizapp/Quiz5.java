9902d0a9f527502694e8efd8f56a12e888feb95e
boupouchi <boupoucchi@linux.boupouchi>
2024-03-29 06:35:33 +0000

    FireStore & Storage

--- app/src/main/java/com/example/quizapp/Quiz5.java
+++ app/src/main/java/com/example/quizapp/Quiz5.java
@@ -1,18 +1,43 @@
 package com.example.quizapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Quiz5 extends AppCompatActivity {
+    FirebaseFirestore fStore;
+    TextView question;
+    FirebaseStorage storage= FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReferenceFromUrl("gs://quizapp-registration.appspot.com").child("q5.jpg");
+    ImageView picture;
     RadioGroup rg;
-    RadioButton rb;
+    RadioButton rb,rb1,rb2;
     Button bNext;
     int score;
     String RepCorrect="Non";
@@ -20,6 +45,39 @@ public class Quiz5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz5);
+        fStore= FirebaseFirestore.getInstance();
+        question= findViewById(R.id.question5);
+        rb1= findViewById(R.id.rb1);
+        rb2= findViewById(R.id.rb2);
+        DocumentReference documentReference = fStore.collection("questions").document("Q5");
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                question.setText(value.getString("question5"));
+                rb1.setText(value.getString("rb1"));
+                rb2.setText(value.getString("rb2"));
+            }
+        });
+        picture = (ImageView) findViewById(R.id.picture5);
+        final File file;
+        try {
+            file = File.createTempFile("picture5","JPG");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        storageRef.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                picture.setImageBitmap(bitmap);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Quiz5.this, "Image failed to load from Storage", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         rg=(RadioGroup) findViewById(R.id.rg);
         bNext=(Button) findViewById(R.id.bNext);
         Intent intent=getIntent();
